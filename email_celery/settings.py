5c5fd0c2812eb540a6919c18b9c9db8b910e6eb8
Anndonajames <56813345+Anndonajames@users.noreply.github.com>
2022-08-22 00:22:04 +0530

    Update settings.py

--- email_celery/settings.py
+++ email_celery/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t6iyw!xog@86%(6(xcy@0=*=qnryb%hzrk)bduq5*gy#8lzwm_'
+SECRET_KEY = '#####################'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
